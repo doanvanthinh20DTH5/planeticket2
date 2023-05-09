9918f1a0e851e4f6280384287c4fd4812bdfb60c
doan van thinh <110151184+doanvanthinh20DTH5@users.noreply.github.com>
2023-05-09 23:03:53 +0700

    Initial Commit

---
+++ planeticket2/planeticket2App.swift
@@ -0,0 +1,17 @@
+//
+//  planeticket2App.swift
+//  planeticket2
+//
+//  Created by DoanThinh on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct planeticket2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
